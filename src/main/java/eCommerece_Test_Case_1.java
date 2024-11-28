cf6f7f8a86a7b971f289ed660956f405b5aea1ac
Maruf Monem <marufmonem@gmail.com>
2024-11-28 10:28:19 +0600

    Form fill up

--- src/main/java/eCommerece_Test_Case_1.java
+++ src/main/java/eCommerece_Test_Case_1.java
@@ -1,6 +1,24 @@
-public class eCommerece_Test_Case_1 {
+import io.appium.java_client.AppiumBy;
+import org.testng.annotations.Test;
 
-    public void fillForm(){
+public class eCommerece_Test_Case_1 extends baseTest {
+
+    @Test
+    public void fillForm() throws InterruptedException {
+        driver.findElement(AppiumBy.id("android:id/text1")).click();
+        scrollToItem("Bangladesh");
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Bangladesh\"]")).click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField")).sendKeys("Maruf");
+        driver.hideKeyboard();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/radioFemale")).click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        Thread.sleep(2000);
+
+
+    }
+
+    public void error_FillForm(){
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop")).click();
 
     }
 }
